a35f83ad0cad70e2b5d8b59ebaab6fafd6cc6c2d
TiaSous <tiagorodriguessousa07@gmail.com>
2024-11-15 14:32:26 +0100

    UI(mediatheque) : Ajouter les boutons des menus et autres (ne marchent pas)
    20m-WIP-#10

--- BitRuisseau/BitRuisseau/Accueil.Designer.cs
+++ BitRuisseau/BitRuisseau/Accueil.Designer.cs
@@ -31,12 +31,18 @@
             panel1 = new Panel();
             button1 = new Button();
             LocalFileView = new DataGridView();
+            button2 = new Button();
+            button3 = new Button();
+            SearchBar = new TextBox();
+            button4 = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)LocalFileView).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
             panel1.Controls.Add(button1);
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
@@ -47,7 +53,7 @@
             // 
             button1.Location = new Point(41, 35);
             button1.Name = "button1";
-            button1.Size = new Size(187, 52);
+            button1.Size = new Size(221, 52);
             button1.TabIndex = 0;
             button1.Text = "Accueil";
             button1.UseVisualStyleBackColor = true;
@@ -55,16 +61,52 @@
             // LocalFileView
             // 
             LocalFileView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            LocalFileView.Location = new Point(361, 230);
+            LocalFileView.Location = new Point(361, 188);
             LocalFileView.Name = "LocalFileView";
-            LocalFileView.Size = new Size(861, 412);
+            LocalFileView.Size = new Size(861, 454);
             LocalFileView.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(41, 104);
+            button2.Name = "button2";
+            button2.Size = new Size(221, 52);
+            button2.TabIndex = 1;
+            button2.Text = "Explorer";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(41, 173);
+            button3.Name = "button3";
+            button3.Size = new Size(221, 52);
+            button3.TabIndex = 2;
+            button3.Text = "Importer";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // SearchBar
+            // 
+            SearchBar.Location = new Point(361, 51);
+            SearchBar.Name = "SearchBar";
+            SearchBar.Size = new Size(472, 23);
+            SearchBar.TabIndex = 2;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1052, 135);
+            button4.Name = "button4";
+            button4.Size = new Size(170, 47);
+            button4.TabIndex = 3;
+            button4.Text = "Delete all";
+            button4.UseVisualStyleBackColor = true;
             // 
             // Accueil
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1264, 681);
+            Controls.Add(button4);
+            Controls.Add(SearchBar);
             Controls.Add(LocalFileView);
             Controls.Add(panel1);
             Name = "Accueil";
@@ -72,6 +114,7 @@
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)LocalFileView).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -79,5 +122,9 @@
         private Panel panel1;
         private Button button1;
         private DataGridView LocalFileView;
+        private Button button3;
+        private Button button2;
+        private TextBox SearchBar;
+        private Button button4;
     }
 }
