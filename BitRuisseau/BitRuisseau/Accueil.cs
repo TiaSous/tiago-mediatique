28e50e58ad08c23aeb7395c2005d188a113129d4
TiaSous <tiagorodriguessousa07@gmail.com>
2024-11-15 15:29:02 +0100

    feat(mediatheque) : Ajouter des tags
    30m-WIP-#4
    
    J'affiche 2 nouveaux éléments : le nom de l'artiste et la durée. Pour l'instant ce n'est que pour les mp3. Je ne sais pas encore exactement quelles sont les informations à retenir

--- BitRuisseau/BitRuisseau/Accueil.cs
+++ BitRuisseau/BitRuisseau/Accueil.cs
@@ -1,5 +1,6 @@
 using BitRuisseau.Classes;
 using System.IO;
+using TagLib;
 
 namespace BitRuisseau
 {
@@ -13,14 +14,28 @@ namespace BitRuisseau
             {
                 Directory.CreateDirectory("C:\\BitRuisseau");
             }
-            List<FileInfo> files = Directory.GetFiles("C:\\BitRuisseau").Select(path => new FileInfo(path)).ToList();
+            string[] paths = Directory.GetFiles("C:\\BitRuisseau");
 
             List<MediaData> list = new List<MediaData>();
 
-            foreach (FileInfo file in files)
+            foreach (string path in paths)
             {
-                list.Add(new MediaData { File_duration = 0, File_name = file.Name, File_size = (int)file.Length, File_type = file.Extension });
+                MediaData data = new MediaData();
+                var tfile = TagLib.File.Create(path);
+
+                FileInfo fileInfo = new FileInfo(path);
+                data.File_size = fileInfo.Length;
+
+                data.File_name = tfile.Tag.Title;
+                data.File_type = Path.GetExtension(path);
+                data.File_artist = tfile.Tag.FirstPerformer;
+                TimeSpan duration = tfile.Properties.Duration;
+                data.File_duration = $"{duration.Minutes:D2}:{duration.Seconds:D2}";
+                list.Add(data);
             }
+
+
+
 
 
             LocalFileView.DataSource = list;
